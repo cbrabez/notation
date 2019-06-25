15576cc6e1af816cd395497f8b0bfd87f841470d
Brabez Christian <U940040@sdvrz.de>
2019-06-25 12:26:02 +0200

    notes API complete

--- server.js
+++ server.js
@@ -1,80 +1,40 @@
-// server.js
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// BASE SETUP
-// =============================================================================
+// create express app
+const app = express();
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var User     = require('./models/user');
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// DATABASE SETUP
-// =============================================================================
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
 
-var mongoose   = require('mongoose');
-mongodb://cbrabez:password12345@ds343217.mlab.com:43217/notation
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+mongoose.Promise = global.Promise;
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
-// more routes for our API will happen here
+// Require Notes routes
+require('./routes/note.routes.js')(app);
 
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/users')
-
-    // create a user (accessed at POST http://localhost:8080/api/users)
-    .post(function(req, res) {
-
-        var user = new User();      // create a new instance of the User model
-        user.email = req.body.email;  // set the users name (comes from the request)
-        user.password = req.body.password;
-
-        // save the user and check for errors
-        user.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'User created!' });
-        })
-
-    })
-    .get(function(req, res) {
-        User.find(function(err, users) {
-            if (err)
-                res.send(err);
-
-            res.json(users);
-        });
-    });
-
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
