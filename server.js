5c9da2d130679af0aff9884a2ea1c8a1b9b664af
Christian Brabez <cbrabez@ChristiansMBP2.fritz.box>
2019-06-25 09:53:37 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,80 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var User     = require('./models/user');
+
+// DATABASE SETUP
+// =============================================================================
+
+var mongoose   = require('mongoose');
+mongodb://cbrabez:password12345@ds343217.mlab.com:43217/notation
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/users')
+
+    // create a user (accessed at POST http://localhost:8080/api/users)
+    .post(function(req, res) {
+
+        var user = new User();      // create a new instance of the User model
+        user.email = req.body.email;  // set the users name (comes from the request)
+        user.password = req.body.password;
+
+        // save the user and check for errors
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'User created!' });
+        })
+
+    })
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err)
+                res.send(err);
+
+            res.json(users);
+        });
+    });
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
