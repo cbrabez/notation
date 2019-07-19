2b9ca692d1701f64f2b926d0031e17b3b16341e7
Brabez Christian <U940040@sdvrz.de>
2019-07-19 12:32:26 +0200

    config changed

--- config/config.js
+++ config/config.js
@@ -1,8 +1,3 @@
-/*mdule.exports = {
-    url: 'mongodb://localhost:27017/notation'
-    //url: 'mongodb://cbrabez:password123456@ds343217.mlab.com:43217/notation'
-}
-*/
 module.exports = {
   port: process.env.PORT,
   database: process.env.DB
